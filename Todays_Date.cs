f927d16a404375bf325482f16403353f10d009c7
liangalice <55155166+liangalice@users.noreply.github.com>
2020-01-18 16:27:15 -0800

    added date script

---
+++ Todays_Date.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Todays_Date : MonoBehaviour
+{
+ 
+    [SerializeField] Text todaysDate;
+
+    string timeAndDate = System.DateTime.Now.ToString("MM/dd/y");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetString("date", timeAndDate);
+        todaysDate.text = "Today's date: "+ timeAndDate;
+
+    }
+    void Update()
+    {
+        //   countdownText = timeAndDate;
+    }
+    public void addDateAndDataToLog()
+    {
+     //   GameObject newLog = (GameObject)Instantiate(Resources.Load("logTheDateAndOunce"));
+
+    }
+}
